357cb648d66e0a9c5dad3cb3ef2d9ce5e9ecde44
雷威 <leiwei2094@gmail.com>
2018-05-03 18:09:44 +0800

    commit

--- protobuf/src/test/java/com/leibangzhu/javatutorial/protobuf/ProtobufTest.java
+++ protobuf/src/test/java/com/leibangzhu/javatutorial/protobuf/ProtobufTest.java
@@ -5,54 +5,78 @@ import org.junit.Test;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 
+import com.leibangzhu.javatutorial.protobuf.AddressBookProtos.AddressBook;
+import com.leibangzhu.javatutorial.protobuf.AddressBookProtos.Person;
+
 public class ProtobufTest {
 
     private final String fileName = "/Users/leiwei/tmp/person.protobuf";
 
     @Test
     public void test_write_to_file() throws Exception{
-        AddressBookProtos.AddressBook.Builder addressBook = AddressBookProtos.AddressBook.newBuilder();
 
-        AddressBookProtos.Person.Builder tom = AddressBookProtos.Person.newBuilder();
-        tom.setId(0);
-        tom.setName("tom");
-        tom.setEmail("tom@gmail.com");
-        AddressBookProtos.Person.PhoneNumber.Builder phoneNumberTom = AddressBookProtos.Person.PhoneNumber.newBuilder();
-        phoneNumberTom.setNumber("13588888888");
-        phoneNumberTom.setType(AddressBookProtos.Person.PhoneType.MOBILE);
-        tom.addPhones(phoneNumberTom);
-
-        AddressBookProtos.Person.Builder jerry = AddressBookProtos.Person.newBuilder();
-        jerry.setId(1);
-        jerry.setName("jerry");
-        jerry.setEmail("jerry@163.com");
-        AddressBookProtos.Person.PhoneNumber.Builder phoneNumberJerry = AddressBookProtos.Person.PhoneNumber.newBuilder();
-        phoneNumberJerry.setNumber("13581234520");
-        phoneNumberJerry.setType(AddressBookProtos.Person.PhoneType.MOBILE);
-        jerry.addPhones(phoneNumberJerry);
-
-        addressBook.addPeople(tom);
-        addressBook.addPeople(jerry);
+        AddressBook addressBook = create_address_book();
 
         FileOutputStream output = new FileOutputStream(fileName);
-        addressBook.build().writeTo(output);
+        addressBook.writeTo(output);
     }
 
     @Test
     public void test_read_from_file() throws Exception{
 
-        AddressBookProtos.AddressBook addressBook =
-                AddressBookProtos.AddressBook.parseFrom(new FileInputStream(fileName));
+        AddressBook addressBook = AddressBook.parseFrom(new FileInputStream(fileName));
 
-        for (AddressBookProtos.Person person : addressBook.getPeopleList()){
+        print(addressBook);
+    }
+
+    @Test
+    public void test_write_to_read_from_bytes() throws Exception {
+        AddressBook addressBook = create_address_book();
+
+        byte[] data = addressBook.toByteArray();
+
+        AddressBook addressBook2 = AddressBook.parseFrom(data);
+        print(addressBook2);
+    }
+
+    private AddressBook create_address_book(){
+        AddressBook.Builder addressBook = AddressBook.newBuilder();
+
+        Person.Builder tom = Person.newBuilder();
+        tom.setId(0);
+        tom.setName("tom");
+        tom.setEmail("tom@gmail.com");
+        Person.PhoneNumber.Builder phoneNumberTom = Person.PhoneNumber.newBuilder();
+        phoneNumberTom.setNumber("13588888888");
+        phoneNumberTom.setType(Person.PhoneType.MOBILE);
+        tom.addPhones(phoneNumberTom);
+
+        Person.Builder jerry = Person.newBuilder();
+        jerry.setId(1);
+        jerry.setName("jerry");
+        jerry.setEmail("jerry@163.com");
+        Person.PhoneNumber.Builder phoneNumberJerry = Person.PhoneNumber.newBuilder();
+        phoneNumberJerry.setNumber("13581234520");
+        phoneNumberJerry.setType(Person.PhoneType.MOBILE);
+        jerry.addPhones(phoneNumberJerry);
+
+        addressBook.addPeople(tom);
+        addressBook.addPeople(jerry);
+
+        return addressBook.build();
+    }
+
+    private void print(AddressBook addressBook){
+        for (Person person : addressBook.getPeopleList()){
             System.out.println("========================");
             System.out.println(person.getId());
             System.out.println(person.getName());
             System.out.println(person.getEmail());
-            for (AddressBookProtos.Person.PhoneNumber phoneNumber : person.getPhonesList()){
+            for (Person.PhoneNumber phoneNumber : person.getPhonesList()){
                 System.out.println(phoneNumber.getNumber());
                 System.out.println(phoneNumber.getType());
             }
         }
     }
+
 }
