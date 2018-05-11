0a918d8fd2cb05327ceafd9efdcb09f16510a220
雷威 <leiwei2094@gmail.com>
2018-05-11 10:01:36 +0800

    commit

--- dubbo/provider/src/main/java/com/leibangzhu/javatutorial/dubbo/provider/ProviderApp.java
+++ dubbo/provider/src/main/java/com/leibangzhu/javatutorial/dubbo/provider/ProviderApp.java
@@ -2,9 +2,7 @@ package com.leibangzhu.javatutorial.dubbo.provider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @SpringBootApplication
 public class ProviderApp {
 
